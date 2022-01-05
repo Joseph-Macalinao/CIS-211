e09f3f13e098abfdb3eb04436f1e1820e686ba67
Joseph Macalinao <jhmac121101@gmail.com>
2022-01-05 12:48:19 -0800

    minke

--- Project 1 - Solar System/planet.py
+++ Project 1 - Solar System/planet.py
@@ -11,53 +11,53 @@ class Planet:
         param mass: mass of the object
         param dist: distance of the object from the Sun
         '''
-        self.__name = name
-        self.__radius = rad
-        self.__mass = mass
-        self.__distance = dist
+        self.name = name
+        self.radius = rad
+        self.mass = mass
+        self.distance = dist
 
     def get_name(self):
         '''
         returns the name of given object
         return: name of object
         '''
-        return self.__name
+        return self.name
 
     def get_radius(self):
         '''
         returns radius of given object
         return: radius of object
         '''
-        return self.__radius
+        return self.radius
 
     def get_mass(self):
         '''
         returns mass of given object
         return: mass of object
         '''
-        return self.__mass
+        return self.mass
 
     def get_distance(self):
         '''
         returns distance from sun of given object
         return: distance of given object
         '''
-        return self.__distance
+        return self.distance
 
     def get_volume(self):
         '''
         gives the volume of the given planet object
         return:
         '''
-        volume = 4/3 * math.pi * self.__radius ** 3
+        volume = 4/3 * math.pi * self.radius ** 3
         return volume
 
     def get_surface_area(self):
-        sa = 4 * math.pi * self.__radius ** 2
+        sa = 4 * math.pi * self.radius ** 2
         return sa
 
     def get_density(self):
-        density = self.__mass / self.get_volume()
+        density = self.mass / self.get_volume()
         return density
 
     def set_name(self, new_name):
--- Project 1 - Solar System/test_planet.py
+++ Project 1 - Solar System/test_planet.py
@@ -7,13 +7,13 @@ class T0_TestPlanet(unittest.TestCase):
 
     def test_planet_build(self):
         p1 = planet.Planet('x25', 10.0, 100.0, 50.0)
-        self.assertEqual(p1.__name, 'x25')
-        self.assertEqual(p1.__rad, 10.0)
-        self.assertEqual(p1.__mass, 100.0)
-        self.assertEqual(p1.__dist, 50.0)
+        self.assertEqual(p1.name, 'x25')
+        self.assertEqual(p1.radius, 10.0)
+        self.assertEqual(p1.mass, 100.0)
+        self.assertEqual(p1.distance, 50.0)
 
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
 
-0
+
